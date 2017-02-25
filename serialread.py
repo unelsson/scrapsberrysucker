17b4d574f7ce13de406845b0c7b0f2c8e1b874ce
unelsson <unelsson@gmail.com>
2017-02-25 16:40:42 +0200

    Update serialread.py

--- serialread.py
+++ serialread.py
@@ -1,16 +1,14 @@
  #!/usr/bin/env python
-          
       
-           import time
-           import serial
+import time
+import serial
        
       
-           readserial = serial.Serial(
-              
-               port='/dev/ttyUSB0',
-               baudrate = 9600,
-               parity=serial.PARITY_NONE,
-               stopbits=serial.STOPBITS_ONE,
-               bytesize=serial.EIGHTBITS,
-               timeout=1
-           )
+readserial = serial.Serial(
+  port='/dev/ttyUSB0',
+  baudrate = 9600,
+  parity=serial.PARITY_NONE,
+  stopbits=serial.STOPBITS_ONE,
+  bytesize=serial.EIGHTBITS,
+  timeout=1
+  )
