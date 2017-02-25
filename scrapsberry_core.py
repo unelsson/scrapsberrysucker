d7071149074baf334b3a5bb437c4f4a4f7b04331
unelsson <unelsson@gmail.com>
2017-02-25 17:30:34 +0200

    Update scrapsberry_core.py

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -13,8 +13,7 @@ while 1:
     x = int(x)
     if int(x) >= 0 & int(x) <= 500:
       print(x);
-      for i in range(0, x):
-        i=i+50;
+      for i in range(0, x, 25):
         print ('#', end='');
   except ValueError:
     print('Invalid read');
