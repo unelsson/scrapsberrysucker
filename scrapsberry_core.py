473a1121c319750989840761923a7e52bae30732
unelsson <unelsson@gmail.com>
2017-02-25 17:00:37 +0200

    Update scrapsberry_core.py
    text-based graphics for sensor read

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
+import os
 import serialread
 
 while 1:
+  os.system('clear');
   x = serialread.ser.readline();
-  print x;
+  for i in range(0, x):
+    x=x+25;
+    printf "#";
+  print "";
