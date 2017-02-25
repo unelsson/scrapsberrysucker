4d17df7c06a8726e031fcd1b168fa24ff4835f5a
unelsson <unelsson@gmail.com>
2017-02-25 17:10:57 +0200

    Update scrapsberry_core.py

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -8,9 +8,9 @@ while 1:
   try:
     x = int(x)
     if int(x) >= 0 & int(x) <= 256:
-    for i in range(0, x):
-      x=x+25;
-      printf("#");  
+      for i in range(0, x):
+        x=x+25;
+        printf("#");  
   except ValueError:
     print "Invalid read"
   print '';
