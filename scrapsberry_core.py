50d41df34c140cb9c7e00bb6e2abd9102f0d0808
unelsson <unelsson@gmail.com>
2017-02-27 02:39:34 +0200

    Update scrapsberry_core.py

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -10,19 +10,19 @@ while 1:
   x = serialread.ser.readline()
   print(x)
   k = cv2.waitKey(33)
-    if k==2490368:
-      print('UP')
-    if k==2424832:
-      print('LEFT')
-    if k==2555904:
-      print('RIGHT')
-    if k==2621440:
-      print('DOWN')
-    time.sleep(1)
-    x = int(x)
-    if int(x) >= 0 & int(x) <= 500:
-      for i in range(0, x, 25):
-        print ('#', end='')
+  if k==2490368:
+    print('UP')
+  if k==2424832:
+    print('LEFT')
+  if k==2555904:
+    print('RIGHT')
+  if k==2621440:
+    print('DOWN')
+  time.sleep(1)
+  x = int(x)
+  if int(x) >= 0 & int(x) <= 500:
+    for i in range(0, x, 25):
+      print ('#', end='')
   except ValueError:
     print('Invalid read')
     time.sleep(0.5)
