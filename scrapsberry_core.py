c6cf90d45a6e8324b567c4699b13f53d9d6d0ff2
unelsson <unelsson@gmail.com>
2017-02-28 21:29:35 +0200

    Update scrapsberry_core.py

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -20,7 +20,7 @@ def read_ch():
 
 while 1:
   serread = serialrw.ser.readline()
-  serialrw.ser.reset_input_buffer()
+  serialrw.ser.flushInput()
   print("Non-decoded:", serread)
   print("Decoded", serread.decode('ascii'))
   ch = read_ch()
