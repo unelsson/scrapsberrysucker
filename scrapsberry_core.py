6c4bf24b5b2e4efb1065561965cd43449e16772b
unelsson <unelsson@gmail.com>
2017-02-28 22:51:47 +0200

    pygame -> Kbhit

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -7,8 +7,88 @@ import serialrw
 import time
 import cv2
 import numpy as np
-import pygame
-from pygame.locals import *
+import atexit
+from select import select
+
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+
+        else:
+            return sys.stdin.read(1)
+
+
+    def getarrow(self):
+        ''' Returns an arrow-key code after kbhit() has been called. Codes are
+        0 : up
+        1 : right
+        2 : down
+        3 : left
+        Should not be called in the same program as getch().
+        '''
+
+        if os.name == 'nt':
+            msvcrt.getch() # skip 0xE0
+            c = msvcrt.getch()
+            vals = [72, 77, 80, 75]
+
+        else:
+            c = sys.stdin.read(3)[2]
+            vals = [65, 67, 66, 68]
+
+        return vals.index(ord(c.decode('utf-8')))
+
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
 
 def read_ch():
   fd = sys.stdin.fileno()
@@ -20,24 +100,20 @@ def read_ch():
     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
   return ch
 
-def read_pgch():
-  for event in pygame.event.get():
-    if (event.type == KEYDOWN):
-      print(event)
-      if (event.key == K_q):
-        return 'q'
-      if (event.key == K_g):
-        return 'g'
-    else:
-      return '-1'
+def read_ch_kbhit():
+  kb = KBHit()
+  if kb.kbhit():
+    c = kb.getch()
+    #if ord(c) == 27: # ESC
+    print(c)
+    kb.set_normal_term()
+    return c
+
 
 def main():
-  os.environ["SDL_VIDEODRIVER"] = "dummy"
-  pygame.init()
-  pygame.display.set_mode((1,1))
   while 1:
-    #ch = read_ch()
-    ch = read_pgch()
+    ch = read_ch()
+    read_ch_kbhit()
     print(ch)
     serialrw.ser.write(bytes(ch, 'ascii')); # Send pressed character to Arduino as bytes
     if ch=="q":  
