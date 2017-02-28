6ab0bb6010639fb74c6d2a55f900f1eaf4ea8ab0
unelsson <unelsson@gmail.com>
2017-02-28 20:02:39 +0200

    Update scrapsberry_core.py

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -24,21 +24,7 @@ while 1:
   print("Decoded", x.decode('ascii'))
   ch = read_ch()
   print(ch)
-  if ch=="w":
-    print("Pressed w")
-    serialrw.ser.write(b'w')
-  if ch=="a":
-    print("Pressed a")
-    serialrw.ser.write(b'a')
-  if ch=="s":
-    print("Pressed s")
-    serialrw.ser.write(b's')
-  if ch=="d":
-    print("Pressed d")
-    serialrw.ser.write(b'd')
-  if ch=="x":
-    print("Pressed x")
-    serialrw.ser.write(b'x')
+  serialrw.ser.write(bytes(ch)); // Send pressed character to Arduino as bytes
   if ch=="q":  
     break
   ## try:
