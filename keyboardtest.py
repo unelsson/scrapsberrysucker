6bea180763589c939c338db2e43a72d1f03dcb53
unelsson <unelsson@gmail.com>
2017-02-28 21:33:13 +0000

    cleanup

---
+++ keyboardtest.py
@@ -0,0 +1,28 @@
+import sys
+import select
+import tty
+import termios
+
+class NonBlockingConsole(object):
+
+    def __enter__(self):
+        self.old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        return self
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+
+    def get_data(self):
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            return sys.stdin.read(1)
+        return False
+
+with NonBlockingConsole() as nbc:
+    i = 0
+    while 1:
+        print(i)
+        i += 1
+        if nbc.get_data() == '\x1b':  # x1b is ESC
+            break
