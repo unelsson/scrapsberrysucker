736485c7bc555016f5b2810c3b384cc8a665c4ef
unelsson <unelsson@gmail.com>
2017-03-10 00:16:07 +0000

    particlefilter_randoms

--- scrapsberry_core.py
+++ scrapsberry_core.py
@@ -8,7 +8,10 @@ import time
 import cv2
 import numpy as np
 import math
+import random
 import urllib.request
+
+random.seed()
 
 serreaddata = ['/0']*19 #List for storing latest IR scan data
 irdistdata = [0]*19 #Latest IR scan data converted to distances
@@ -23,7 +26,7 @@ visiony = 0
 robotangle = 0.000
 
 particles = 25 #Amount of particles
-particlefilter = np.array([[robotx, roboty, robotangle, 0.5]]*particles)
+particlefilter = np.array([[round(robotx), round(roboty), robotangle, 0.5]]*particles)
  
 # This code was modified from pyimagesearch.com
 # Test code for opening a snapshot to opencv2 from mjpg-streamer
@@ -33,6 +36,7 @@ particlefilter = np.array([[robotx, roboty, robotangle, 0.5]]*particles)
 #cv2.imwrite('camerass.jpg',image)
 
 ch = 0 #Keyboard input
+
 
 def read_ch():
     fd = sys.stdin.fileno()
@@ -44,12 +48,76 @@ def read_ch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
+
+def readarduinoserial():
+    for i in range(0, 19):
+        serread = serialrw.ser.readline()
+        serreaddata[i] = int(serread.decode('ascii').strip('\r\n'))
+        if serreaddata[i] < 70: irdistdata[i]=10
+        if serreaddata[i] <= 96 & serreaddata[i] > 70: irdistdata[i]=4
+        if serreaddata[i] <= 120 & serreaddata[i] > 96: irdistdata[i]=3
+        if serreaddata[i] <= 180 & serreaddata[i] > 120: irdistdata[i]=2
+        if serreaddata[i] <= 235 & serreaddata[i] > 180: irdistdata[i]=2
+        if serreaddata[i] <= 250 & serreaddata[i] > 235: irdistdata[i]=1
+        if serreaddata[i] > 250: irdistdata[i]=1
+        # Interpreting look() values (Sharp IR mean average)
+        # 60-70 (free view, almost)
+        #   96 (2 * A4 paper)
+        #   120 (1,5 * A4 paper)
+        #   180 (1 * A4 paper)
+        #   235 (0,5 * A4 paper)
+        print('debug1')
+
+
+def drawmap():
+    for i in range(0, 19):
+        if int(irdistdata[i]) > 0:
+            for k in range(0, irdistdata[i]+1):
+                drawpointerx = round(robotx + math.sin(robotangle-1.57+i*0.174)*k)
+                drawpointery = round(roboty + math.cos(robotangle-1.57+i*0.174)*k)
+                temporaryb = map[drawpointerx, drawpointery,0]
+                temporaryg = map[drawpointerx, drawpointery,1]
+                temporaryr = map[drawpointerx, drawpointery,2]
+                lightenedb = temporaryb + 70
+                lightenedg = temporaryg + 70
+                lightenedr = temporaryr + 70
+                darkenedb = temporaryb - 30
+                darkenedg = temporaryg - 30
+                darkenedr = temporaryr - 30
+                if lightenedb > 255: lightenedb = 255
+                if lightenedg > 255: lightenedg = 255
+                if lightenedr > 255: lightenedr = 255
+                if darkenedb > 255: darkenedb = 255
+                if darkenedg > 255: darkenedg = 255
+                if darkenedr > 255: darkenedr = 255
+                if lightenedb < 0: lightenedb = 0
+                if lightenedg < 0: lightenedg = 0  
+                if lightenedr < 0: lightenedr = 0
+                if darkenedb < 0: darkenedb = 0
+                if darkenedg < 0: darkenedg = 0
+                if darkenedr < 0: darkenedr = 0
+                if drawpointerx > 0 and drawpointerx < mapsize and drawpointery > 0 and drawpointery < mapsize:
+                    if k < irdistdata[i]: map[(drawpointerx, drawpointery)] = [darkenedb,darkenedg,darkenedr]
+                    if k == irdistdata[i] and irdistdata[i] < 9: map[(drawpointerx, drawpointery)] = [lightenedb,lightenedg,lightenedr]
+
+
 def moveparticles(move, turn):     #move forward in cm*10, turn right in radian
     for i in range(0, particles):
         particlefilter[i,2] = particlefilter[i,2] + turn
-        particlefilter[i,0] = particlefilter[i,0] + math.sin(particlefilter[i,2])*move
-        particlefilter[i,1] = particlefilter[i,1] + math.cos(particlefilter[i,2])*move
+        particlefilter[i,0] = int(particlefilter[i,0] + math.sin(particlefilter[i,2])*move)
+        particlefilter[i,1] = int(particlefilter[i,1] + math.cos(particlefilter[i,2])*move)
         particlefilter[i,3] = particlefilter[i,3]
+
+
+def updateparticles():
+    for i in range(0, particles):
+        particlefilter[i,2] = particlefilter[i,2] + (random.random()-0.5)/10
+        particlefilter[i,0] = int(particlefilter[i,0] + random.random()*10-5)
+        particlefilter[i,1] = int(particlefilter[i,1] + random.random()*10-5)
+        particlefilter[i,3] = particlefilter[i,3] + random.random()-0.5
+
+def resampleparticles():
+    return np.flipud(particlefilter[np.argsort(particlefilter[:, 3])])
 
 while 1:
     print('You pressed',ch)
@@ -62,15 +130,19 @@ while 1:
     if ch=="w":
         serialrw.ser.write(b'w')
         moveparticles(2, 0)       #20cm, 0 angle turn
+        updateparticles()
     if ch=="a":
         serialrw.ser.write(b'a')
         moveparticles(0, -0.52)   #-30 degrees
+        updateparticles()
     if ch=="s":
         serialrw.ser.write(b's')
         moveparticles(-1, 0)      #-10cm, 0 angle turn
+        updateparticles()
     if ch=="d":
         serialrw.ser.write(b'd')
         moveparticles(0, 0.52)    #+30 degrees
+        updateparticles()
     if ch=="q":  
         break
     if ch=="g":
@@ -80,55 +152,10 @@ while 1:
         serialrw.ser.write(b'g')
         time.sleep(2.5)
         try:
-            for i in range(0, 19):
-                serread = serialrw.ser.readline()
-                serreaddata[i] = int(serread.decode('ascii').strip('\r\n'))
-                if serreaddata[i] < 70: irdistdata[i]=10
-                if serreaddata[i] <= 96 & serreaddata[i] > 70: irdistdata[i]=4
-                if serreaddata[i] <= 120 & serreaddata[i] > 96: irdistdata[i]=3
-                if serreaddata[i] <= 180 & serreaddata[i] > 120: irdistdata[i]=2
-                if serreaddata[i] <= 235 & serreaddata[i] > 180: irdistdata[i]=2
-                if serreaddata[i] <= 250 & serreaddata[i] > 235: irdistdata[i]=1
-                if serreaddata[i] > 250: irdistdata[i]=1
-                # Interpreting look() values (Sharp IR mean average)
-                # 60-70 (free view, almost)
-                #   96 (2 * A4 paper)
-                #   120 (1,5 * A4 paper)
-                #   180 (1 * A4 paper)
-                #   235 (0,5 * A4 paper)
-                print('debug1')
+            readarduinoserial()
         except ValueError:
-            print('Error! :)')
-        for i in range(0, 19):
-            if int(irdistdata[i]) > 0 and int(irdistdata[i]) < 7:
-                for k in range(0, irdistdata[i]+1):
-                    drawpointerx = round(robotx + math.sin(robotangle-1.57+i*0.174)*k)
-                    drawpointery = round(roboty + math.cos(robotangle-1.57+i*0.174)*k)
-                    temporaryb = map[drawpointerx, drawpointery,0]
-                    temporaryg = map[drawpointerx, drawpointery,1]
-                    temporaryr = map[drawpointerx, drawpointery,2]
-                    lightenedb = temporaryb + 70
-                    lightenedg = temporaryg + 70
-                    lightenedr = temporaryr + 70
-                    darkenedb = temporaryb - 70
-                    darkenedg = temporaryg - 70
-                    darkenedr = temporaryr - 70
-                    if lightenedb > 255: lightenedb = 255
-                    if lightenedg > 255: lightenedg = 255
-                    if lightenedr > 255: lightenedr = 255
-                    if darkenedb > 255: darkenedb = 255
-                    if darkenedg > 255: darkenedg = 255
-                    if darkenedr > 255: darkenedr = 255
-                    if lightenedb < 0: lightenedb = 0
-                    if lightenedg < 0: lightenedg = 0  
-                    if lightenedr < 0: lightenedr = 0
-                    if darkenedb < 0: darkenedb = 0
-                    if darkenedg < 0: darkenedg = 0
-                    if darkenedr < 0: darkenedr = 0
-                    if drawpointerx > 0 and drawpointerx < mapsize and drawpointery > 0 and drawpointery < mapsize:
-                        if k < irdistdata[i]: map[(drawpointerx, drawpointery)] = [darkenedb,darkenedg,darkenedr]
-                        if k == irdistdata[i] and irdistdata[i] < 7: map[(drawpointerx, drawpointery)] = [lightenedb,lightenedg,lightenedr]
-                    print('debug2')
+            print('Read error! :)')
+        drawmap()
         cv2.imwrite('robomap.png', map)
-
+    particlefilter = resampleparticles()
   
